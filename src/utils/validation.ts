63c3c24995af1213c7c2ba357670451692f9ed7d
Rohith Sv <svrohith9@gmail.com>
2023-09-17 02:11:27 -0500

    Loose couple the login into other files

---
+++ src/utils/validation.ts
@@ -0,0 +1,6 @@
+// src/utils/validation.ts
+export function validateEmail(email: string): boolean {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return emailRegex.test(email);
+  }
+  
